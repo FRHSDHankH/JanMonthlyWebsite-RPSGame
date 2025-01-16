3bbb640fc023f8f6c03893d4301e9a136d2a0ec2
FRHSDHankH <526hhabashy@frhsd.com>
2025-01-16 10:43:37 -0500

    Stuff idek someone help me

--- script.js
+++ script.js
@@ -30,60 +30,84 @@ function unshake() {
   document.getElementById('imgChange2').classList.remove('shake2')
 }
 
+
+
 function playGame(userChoice) {
   document.getElementById('imgChange1').classList.add('shake1')
   document.getElementById('imgChange2').classList.add('shake2')
+  document.getElementById('imgChange1').src='imgs/Neutral.png'
+  document.getElementById('imgChange2').src='imgs/Neutral.png'
 
-  let computerChoice = getComputerChoice()
+  setTimeout(unshake, 1500)
+  setTimeout(function(){
+    let computerChoice = getComputerChoice()
 
   if (userChoice === computerChoice) {
     console.log("Tie!");
+    if(computerChoice === 'rock') {
+      document.getElementById('imgChange2').src='imgs/Rock.png'
+    }
+    if(computerChoice === 'paper') {
+      document.getElementById('imgChange2').src='imgs/Paper.png'
+    }
+    if(computerChoice === 'scissors') {
+      document.getElementById('imgChange2').src='imgs/Scissors.png'
+    }
   }
   if (userChoice === "rock") {
+    document.getElementById('imgChange1').src='imgs/Rock.png'
     if (computerChoice === "paper") {
       console.log("Computer Won!");
       computerWins()
+      document.getElementById('imgChange2').src='imgs/Paper.png'
     } else if (computerChoice === "scissors") {
       console.log("You Won!");
       userWins()
+      document.getElementById('imgChange2').src='imgs/Scissors.png'
     }
   }
   if (userChoice === "paper") {
+    document.getElementById('imgChange1').src='imgs/Paper.png'
     if (computerChoice === "scissors") {
       console.log("Computer Won!");
       computerWins()
+      document.getElementById('imgChange2').src='imgs/Scissors.png'
     } else if (computerChoice === "rock") {
       console.log("You Won!");
       userWins()
+      document.getElementById('imgChange2').src='imgs/Rock.png'
     } 
   }
   if (userChoice === "scissors") {
+    document.getElementById('imgChange1').src='imgs/Scissors.png'
     if (computerChoice === "rock") {
       console.log("Computer Won!");
       computerWins()
+      document.getElementById('imgChange2').src='imgs/Rock.png'
     } else if (computerChoice === "paper") {
       console.log("You Won!");
       userWins()
+      document.getElementById('imgChange2').src='imgs/Paper.png'
     } 
-  }
-  
+  } 
+
   if(userScore === 1) {
-    document.getElementById('bigComputerHeart3').classList.add('hide')
+    document.getElementById('ComputerHeart3').classList.add('hide')
   }
   if(userScore === 2) {
-    document.getElementById('bigComputerHeart2').classList.add('hide')
+    document.getElementById('ComputerHeart2').classList.add('hide')
   }
   if(userScore === 3) {
-    document.getElementById('bigComputerHeart1').classList.add('hide')
+    document.getElementById('ComputerHeart1').classList.add('hide')
   }
   if(computerScore === 1) {
-    document.getElementById('bigUserHeart3').classList.add('hide')
+    document.getElementById('UserHeart3').classList.add('hide')
   }
   if(computerScore === 2) {
-    document.getElementById('bigUserHeart2').classList.add('hide')
+    document.getElementById('UserHeart2').classList.add('hide')
   }
   if(computerScore === 3) {
-    document.getElementById('bigUserHeart1').classList.add('hide')
+    document.getElementById('UserHeart1').classList.add('hide')
   }
 
   console.log(userScore)
@@ -100,8 +124,9 @@ function playGame(userChoice) {
     userScore = 0
     computerScore = 0
   }
-
-  setTimeout(unshake, 1000)
-
+  }, 1500)
 }
 
+if(userScore===3 || computerScore===3) {
+
+}
